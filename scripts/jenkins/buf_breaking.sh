315293be064243b8857b75c9ca7f2c661ee10c79
kanikamittal09 <94995410+kanikamittal09@users.noreply.github.com>
2022-03-29 21:20:11 +0530

    add rename/move code

--- scripts/jenkins/buf_breaking.sh
+++ scripts/jenkins/buf_breaking.sh
@@ -1,11 +1,39 @@
-EXIT_CODE=0
-for i in $(git log develop..F3 --format=format:%H)
-do 
+#!/bin/bash
+curl -L -o buf1 https://github.com/bufbuild/buf/releases/download/v1.1.0/buf-Linux-x86_64 
+chmod +x buf1 
+./buf1 --version 
+git fetch origin buf_test 
+git checkout buf_test 
+git status 
+EXIT_CODE=0                                 
+
+IFS=$'\//n\/' 
+echo $IFS
+bufOut=($(./buf1 breaking --against '.git#branch=develop'))
+for m in $(git log develop..buf_test --find-renames --format=format:%H  --diff-filter=R)
+do
+    echo $m
+
+    for k in $(git show --pretty="" --name-only $m | grep \.proto$)
+    do
+        echo $k
+        for l in "${bufOut[@]}"
+        do
+            if grep -q "$k" <<< "$l";
+            then
+                echo $l
+            fi
+        done
+    done
+done
+
+for i in $(git log develop..buf_test --format=format:%H)
+do
 echo $i
 for j in $(git show --pretty="" --name-only $i | grep \.proto$)
 do
     echo $j;
-    buf breaking --against '.git#branch=develop' --path $j || TEMP_EXIT_CODE=$? 
+    ./buf1 breaking --against '.git#branch=develop' --path $j || TEMP_EXIT_CODE=$?
     if [ -z "$TEMP_EXIT_CODE" ]
     then
     echo "No Breaking Change"
@@ -14,7 +42,7 @@ do
     then
         EXIT_CODE=$TEMP_EXIT_CODE
         echo $EXIT_CODE
-    fi 
+    fi
     fi
     done
-done 
+done
